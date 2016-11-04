8af3f3cf276b98f92cf9a28ad0f18c85556fe547
raul <raulguti90@gmail.com>
2016-11-04 16:46:38 -0500

    Proyecto-4

--- uniandes/cloud/controller/TemporalFileService.py
+++ uniandes/cloud/controller/TemporalFileService.py
@@ -1,11 +1,7 @@
 import os
 
 class TemporalFileService:
-    url_original = None
-    url_banner = None
     url_converted = None
 
     def __init__(self):
-        self.url_original = os.path.abspath(__file__ + "/../../tmp/original")+"/"
-        self.url_banner = os.path.abspath(__file__ + "/../../tmp/banner")+"/"
-        self.url_converted = os.path.abspath(__file__ + "/../../tmp/converted")+"/"
+        self.url_converted = os.path.abspath(__file__ + "/../../tmp")+"/"
